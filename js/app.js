f2bba908fa6f25a6a025a95de75a49976b5f79d1
guchunwen <595077937@qq.com>
2017-03-28 20:53:25 +0800

    3/28 20;53

--- js/app.js
+++ js/app.js
@@ -1,12 +1,7 @@
 /**
  * Created by guchunwen on 2017/3/28  0028.
  */
-function clear() {
-    Source=document.body.firstChild.data;
-    document.open();
-    document.close();
-    document.body.innerHTML=Source;
-}
-$(document).ready(function () {
-    
-});
+
+window.onload = function () {
+
+};

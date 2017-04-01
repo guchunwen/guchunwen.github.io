866a4a4e51e105c8122d3b5e78608f1b22ba21a5
guchunwen <595077937@qq.com>
2017-04-01 23:36:01 +0800

    4/1 23:35

--- blogsite/js/script.js
+++ blogsite/js/script.js
@@ -1,7 +1,7 @@
 /**
  * Created by hxsd on 2017/3/31.
  */
-$(document).ready(function () {
+$(document).ready(function (e) {
 // //禁用右键
 //     function Click(){
 //
@@ -42,6 +42,37 @@ $(document).ready(function () {
 
 //   内容点击区域结束
 
+
+//    自制滚动条,js原生
+    let scroll = document.querySelector('.main .content .scroll');
+    let con = document.querySelector('.main .content_long');
+    let bigBox = document.querySelector('.main .content');
+
+    scroll.addEventListener('mousedown',onDown);
+    let startY = 0;
+    function onDown(e) {
+        startY = e.offsetY;
+        document.addEventListener('mousemove',onMove);
+        document.addEventListener('mouseup',onUp);
+    }
+    function onMove(e) {
+        if ((e.clientY - startY -226)<0){
+            scroll.style.top = 0;
+            con.style.top = 0;
+        }else if ((e.clientY - startY -226)>215){
+            scroll.style.top = '215px';
+            con.style.top = -(con.offsetHeight - bigBox.offsetHeight)/(bigBox.offsetHeight - scroll.offsetHeight)*215+'px';
+        }else {
+            scroll.style.top = e.clientY - startY -226 +'px';
+            con.style.top = -(con.offsetHeight - bigBox.offsetHeight)/(bigBox.offsetHeight - scroll.offsetHeight)*(e.clientY - startY -226)+'px';
+        }
+    }
+    function onUp(e) {
+        document.removeEventListener('mousemove',onMove);
+        document.removeEventListener('mouseup',onUp);
+    }
+
+//    滚动条结束
 
 
 //footer 二维码

e542fb74c5593b5a08f12cee54f42a3488235e34
guchunwen <595077937@qq.com>
2017-04-01 23:49:01 +0800

    23:48

--- blogsite/js/script.js
+++ blogsite/js/script.js
@@ -48,26 +48,28 @@ $(document).ready(function (e) {
     let con = document.querySelector('.main .content_long');
     let bigBox = document.querySelector('.main .content');
 
-    scroll.addEventListener('mousedown',onDown);
+    scroll.addEventListener('mousedown',onDown); //滚动条监听鼠标按下
     let startY = 0;
     function onDown(e) {
-        startY = e.offsetY;
-        document.addEventListener('mousemove',onMove);
-        document.addEventListener('mouseup',onUp);
+        startY = e.offsetY; //鼠标按下事件触发，鼠标按下位置到当前元素上端的Y坐标
+        document.addEventListener('mousemove',onMove);//监听鼠标移动
+        document.addEventListener('mouseup',onUp);//监听鼠标抬起
     }
     function onMove(e) {
-        if ((e.clientY - startY -226)<0){
+        if ((e.clientY - startY -226)<0){ //限制滚动条移动区域
             scroll.style.top = 0;
             con.style.top = 0;
-        }else if ((e.clientY - startY -226)>215){
+        }else if ((e.clientY - startY -226)>215){//限制滚动条移动区域
             scroll.style.top = '215px';
             con.style.top = -(con.offsetHeight - bigBox.offsetHeight)/(bigBox.offsetHeight - scroll.offsetHeight)*215+'px';
         }else {
-            scroll.style.top = e.clientY - startY -226 +'px';
+            scroll.style.top = e.clientY - startY -226 +'px';//鼠标到浏览器上端的距离 - 鼠标按下时到当前元素上端的距离 - 滚动条上端到浏览器上端的距离 == 滚动条滚动的距离
             con.style.top = -(con.offsetHeight - bigBox.offsetHeight)/(bigBox.offsetHeight - scroll.offsetHeight)*(e.clientY - startY -226)+'px';
+            //计算原理：先求出滚动条每滚动一像素，相应的内容区域移动多少像素，再乘以滚动条滚动距离
+                        //用内容区域可滚动的范围 除以  滚动条滚动的范围
         }
     }
-    function onUp(e) {
+    function onUp(e) { //移除监听
         document.removeEventListener('mousemove',onMove);
         document.removeEventListener('mouseup',onUp);
     }

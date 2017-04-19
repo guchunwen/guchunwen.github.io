9fc0a942beac3072b9f0dc01c961c8f526fe64e1
guchunwen <595077937@qq.com>
2017-04-19 23:46:20 +0800

    4/19 23:46

--- blogsite/js/script.js
+++ blogsite/js/script.js
@@ -24,9 +24,9 @@ $(document).ready(function (e) {
     $('.main .list li').click(function () {
         let n = $(this).index();
         if(num) {
-            $('.main .list li').css({'z-index': '1'}).stop(true,true).animate({'left': '0'}, 300);
+            $('.main .list li').css({'z-index': '1'}).stop(true,true).animate({'left': '0'},300);
             $(this).css({'z-index': '5'});
-            $('.main .content_common').eq(n).stop(true,true).delay(300).animate({'width':'640px'},300).find('.close').delay(300).animate({'opacity':'1'},300);
+            $('.main .content_common').eq(n).stop(true,true).delay(300).animate({'width':'640px'},300,'easeOutBounce').find('.close').delay(300).animate({'opacity':'1'},300);
         }else {
             $('.main .content_common').stop(true,true).animate({'width':'0'},200).find('.close').animate({'opacity':'0'},200);
             for(let i =0;i<4;i++) {
